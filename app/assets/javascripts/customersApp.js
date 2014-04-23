6c760a2cd1aab45101417ab05d89b9408d2e8a3a
skambhampati <skambhampati@rpxcorp.com>
2014-04-23 18:15:17 +0530

    Adding route file

--- app/assets/javascripts/customersApp.js
+++ app/assets/javascripts/customersApp.js
@@ -1,25 +1,2 @@
 app = angular.module("customersApp", ['ngRoute', 'ngResource']);
 
-app.config(function($routeProvider){
-  $routeProvider
-    .when('/', {
-      controller: 'CustomersCtrl',
-      templateUrl: '/assets/angular/templates/customers/index.html'
-    })
-    .when('/addCustomer', {
-      controller: 'CustomersCtrl',
-      templateUrl: '/assets/angular/templates/customers/new.html'
-    })
-    .when('/showCustomer/:customerID', {
-      controller: 'CustomersCtrl',
-      templateUrl: '/assets/angular/templates/customers/show.html'
-    }) 
-    .when('/editCustomer/:customerID', {
-      controller: 'CustomersCtrl',
-      templateUrl: '/assets/angular/templates/customers/edit.html'
-    }) 
-    .otherwise({
-      redirectTo: "/"
-    })
-
-});
